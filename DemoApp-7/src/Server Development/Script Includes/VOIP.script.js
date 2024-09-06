cf0cada9d8f0cdb266b0beeb88c1d00a7306ecb6
guna799 <gunavardhan799@gmail.com>
2024-09-06 12:58:56 +0530

    adding one file

--- DemoApp-7/src/Server Development/Script Includes/VOIP.script.js
+++ DemoApp-7/src/Server Development/Script Includes/VOIP.script.js
@@ -2,6 +2,8 @@ var VOIP = Class.create();
 VOIP.prototype = {
     initialize: function() {
     },
+    // This is file is created by Guna in vs code
+    // adding one file now 
 
     type: 'VOIP'
 };
